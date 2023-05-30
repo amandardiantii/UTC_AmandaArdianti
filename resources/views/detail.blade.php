8896c9f21b4e5565b2fd864adf89a4f4d3029238
amandardiantii <ardiantiamanda@gmail.com>
2023-05-30 11:54:45 +0700

    post2

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <div class="mx-8">
-        <h1>{{ $posts->title }}</h1>
+        <h1 class="text-2xl">{{ $posts->title }}</h1>
         <h1>{{ $posts->category }}</h1>
         <img src="{{ $posts->image }}" alt="">
         <p>{{ $posts->desc }}</p>
